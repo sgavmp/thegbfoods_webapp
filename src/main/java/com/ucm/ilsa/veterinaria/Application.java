78fa7bca275a719847a3b6ac2de9b3421834cba7
Sergio Garcia Alonso <sga.vmp@gmail.com>
2015-03-30 17:14:31 +0200

    Archivo principal de Spring Boot (main) para su ejecucion en un servidor web

---
+++ src/main/java/com/ucm/ilsa/veterinaria/Application.java
@@ -0,0 +1,64 @@
+package com.ucm.ilsa.veterinaria;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import org.springframework.oxm.castor.CastorMarshaller;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import com.google.common.eventbus.EventBus;
+import com.ucm.ilsa.veterinaria.tratamiento.IntfTratamiento;
+
+@SpringBootApplication
+@ComponentScan("com.ucm.ilsa.veterinaria")
+@EnableAsync
+public class Application extends SpringBootServletInitializer implements AsyncConfigurer, CommandLineRunner {
+	
+	@Autowired
+	Environment env;
+	
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+    @Override
+    public void run(String... args) throws Exception {
+    	System.out.println("Deploy on " + env.getProperty("name"));
+    }
+	
+    @Bean
+	@Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setMaxPoolSize(4);
+        taskExecutor.setCorePoolSize(4);
+        taskExecutor.setThreadNamePrefix("ILSAxecutor-");
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+	
+	@Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new SimpleAsyncUncaughtExceptionHandler();
+    }   
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+    
+}
